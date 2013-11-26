aca437302742377ab29cbc2fff5e8145c9bb4601
Kilian Köppchen <kiliankoeppchen@googlemail.com>
2013-11-27 00:54:30 +0100

    Add FPS limiter to main game loop.

--- src/gameengine/GameThread.java
+++ src/gameengine/GameThread.java
@@ -3,6 +3,7 @@ package gameengine;
 import android.graphics.Canvas;
 import android.graphics.Point;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.view.SurfaceHolder;
 
 public class GameThread extends Thread {
@@ -26,7 +27,12 @@ public class GameThread extends Thread {
 
 	@Override
 	public void run() {
-		boolean gameRunning = true;
+	  int FRAMES_PER_SECOND = 60;
+	  int SKIP_TICKS = 1000 / FRAMES_PER_SECOND;
+	  long next_game_tick = SystemClock.uptimeMillis();
+	  long sleep_time = 0;
+	  boolean gameRunning = true;
+	  
 		while(gameRunning)
 		{
 			try {
@@ -34,9 +40,21 @@ public class GameThread extends Thread {
 				Canvas canvas = _surfaceHolder.lockCanvas();	
 				_state.draw(canvas);
 				_surfaceHolder.unlockCanvasAndPost(canvas);
+
+        next_game_tick += SKIP_TICKS;
+        sleep_time = next_game_tick - SystemClock.uptimeMillis();
+        if(sleep_time >= 0) {
+            Thread.sleep(sleep_time);
+        }
+        else {
+            // Shit, we are running behind!
+        }
 			} catch (NullPointerException e) {
 				gameRunning = false;
-			}
+			} catch (InterruptedException e) {
+	      // TODO Auto-generated catch block
+	      e.printStackTrace();
+      }
 		}
 	}
 
