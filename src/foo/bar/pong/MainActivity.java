4d08be461c598496132654215eb412d507ac5b3b
Kilian Köppchen <kiliankoeppchen@googlemail.com>
2013-12-02 16:20:37 +0100

    Enable/Disable hotspot onCreate/OnDestroy for Main
    Activity.

--- src/foo/bar/pong/MainActivity.java
+++ src/foo/bar/pong/MainActivity.java
@@ -1,8 +1,18 @@
 package foo.bar.pong;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 import constants.Values;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -15,6 +25,42 @@ import android.widget.Toast;
 public class MainActivity extends Activity implements OnMenuItemClickListener {
 	
 	private SharedPreferences settings;
+	String temp_text = "";
+	
+	Thread netz_thread = new Thread() {
+			public void run() {
+				try {
+					
+					while(true){
+						// networkstuff
+						int server_port = 4321;
+						byte[] message = new byte[1500];
+						DatagramPacket p = new DatagramPacket(message, message.length);
+						DatagramSocket s = new DatagramSocket(server_port);
+						if(isInterrupted()) {
+							s.close();
+							this.join();
+						}
+						if(!s.isClosed()){
+							s.receive(p);
+						}
+						temp_text = new String(message, 0, p.getLength());
+						if(!s.isClosed()){
+							s.close();
+						}					
+					}
+				} catch (SocketException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +69,14 @@ public class MainActivity extends Activity implements OnMenuItemClickListener {
 		
 		this.settings = getSharedPreferences(Values.CONFIG, MODE_PRIVATE);
 		this.checkForSettings();
+		this.enableHotspot();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		System.out.println("Destroy hotspot");
+		this.stopHotspot();
 	}
 	
 	private void checkForSettings()	{
@@ -88,5 +142,62 @@ public class MainActivity extends Activity implements OnMenuItemClickListener {
 		}
 		return false;
 	}
+	
+	private void enableHotspot() {
+		WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		Method[] wmMethods = wifi.getClass().getDeclaredMethods();
+		wifi.setWifiEnabled(false);
+		for(Method method: wmMethods){
+		  if(method.getName().equals("setWifiApEnabled")){
+		    WifiConfiguration netConfig = new WifiConfiguration();
+		    SharedPreferences settings = this.getSharedPreferences(Values.CONFIG, Context.MODE_PRIVATE);
+		    netConfig.SSID = settings.getString(Values.SSID, "MuscleRecovery");
+		    netConfig.preSharedKey = settings.getString(Values.PKEY, "MuscleRecovery");
+		    netConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+		    netConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+		    netConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+		    netConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+		    netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+		    netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+		    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+		    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);  
 
+		    try {
+		      method.invoke(wifi, netConfig,true);
+		    } catch (IllegalArgumentException e) {
+		      e.printStackTrace();
+		    } catch (IllegalAccessException e) {
+		      e.printStackTrace();
+		    } catch (InvocationTargetException e) {
+		      e.printStackTrace();
+		    }
+		  }
+		}
+	}
+	
+	private void stopHotspot() {
+	    try
+	    {
+	    	WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+	        Method[] wmMethods = wifi.getClass().getDeclaredMethods();
+
+	        for (Method method : wmMethods) {
+	            if (method.getName().equals("setWifiApEnabled")) {
+	                try {
+	                    method.invoke(wifi, null, false);
+	                } catch (IllegalArgumentException e) {
+	                    e.printStackTrace();
+	                } catch (IllegalAccessException e) {
+	                    e.printStackTrace();
+	                } catch (InvocationTargetException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        }
+	    }
+	    catch(Exception e)
+	    {
+	    	e.printStackTrace();
+	    }
+	}
 }
